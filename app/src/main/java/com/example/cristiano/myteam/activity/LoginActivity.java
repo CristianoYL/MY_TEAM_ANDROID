57c6e602ddd08e9116e7cc5e3d52dba76cebc2c0
Li Yin <li.yin.cristiano@rutgers.edu>
2017-03-16 00:59:39 -0400

    minor alteration on login

--- app/src/main/java/com/example/cristiano/myteam/activity/LoginActivity.java
+++ app/src/main/java/com/example/cristiano/myteam/activity/LoginActivity.java
@@ -19,6 +19,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,6 +29,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -321,11 +323,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 String[] pieces = credential.split(":");
                 if (pieces[0].equals(mEmail)) {
                     // Account exists, return true if the password matches.
+                    if ( pieces[1].equals(mPassword) ) {
+                        Log.d("TEST","Login succeeded!");
+                    } else {
+                        Log.d("TEST","Login failed!");
+                    }
                     return pieces[1].equals(mPassword);
                 }
             }
 
             // TODO: register the new account here.
+            Log.d("TEST","Registering...");
             return true;
         }
 
