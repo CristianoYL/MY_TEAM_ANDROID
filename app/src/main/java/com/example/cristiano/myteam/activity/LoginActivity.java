0f392a9d7cf6f13fd832910f914291c7231d0e6a
Li Yin <li.yin.cristiano@rutgers.edu>
2017-03-15 22:45:22 -0400

    Added LoginActivity (blank)

---
+++ app/src/main/java/com/example/cristiano/myteam/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.cristiano.myteam.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cristiano.myteam.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
